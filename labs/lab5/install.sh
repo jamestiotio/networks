8e2c6dcee19644c3393f9e2ea9b7cba67c0543ff
James R T <jamestiotio@gmail.com>
2021-12-01 19:59:10 +0800

    Add useful comments to lab 5 install.sh script

--- labs/lab5/install.sh
+++ labs/lab5/install.sh
@@ -4,7 +4,7 @@
 sudo apt-get install -y quagga curl screen python-setuptools
 sudo easy_install termcolor
 
-# soft-link the conf files to allow students to edit them directly
+# Soft-link the conf files to allow students to edit them directly
 sudo rm /etc/quagga/*
 sudo ln -s $PWD/conf/bgpd-R1.conf /etc/quagga/
 sudo ln -s $PWD/conf/bgpd-R2.conf /etc/quagga/
@@ -16,7 +16,9 @@ sudo ln -s $PWD/conf/zebra-R3.conf /etc/quagga/
 sudo ln -s $PWD/conf/zebra-R4.conf /etc/quagga/
 sudo chown quagga /etc/quagga/*.conf
 
-# create /var/run/quagga, which mysteriously is not created by install
+# Create /var/run/quagga, which is sometimes mysteriously not created by install
 # Without it, zebra will not update host routes
+# Note that this folder is ephemeral, so it will be deleted on a hard reboot (re-execute these 2 lines after a reboot)
+# Since the routing tables are stored/updated by zebra here, without this folder, inter-AS communication would be impossible (i.e., `Destination Net Unreachable` errors during pings)
 sudo mkdir /var/run/quagga
 sudo chown quagga /var/run/quagga
